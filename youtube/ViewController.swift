f44bd0a7c2dbcdc7b06c955fd4f5a3c0d835a55c
Victor <vquesnel@student.42.fr>
2018-04-26 14:14:18 +0200

    First commit

---
+++ youtube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube
+//
+//  Created by victor quesnel on 26/04/2018.
+//  Copyright © 2018 victor quesnel. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Home"
+        collectionView?.backgroundColor = UIColor.white
+    }
+}
+
